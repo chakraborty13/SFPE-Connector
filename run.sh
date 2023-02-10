a1ea5f986154cabd273607a741566088426f39c3
Satyajit Chakraborty <sa.chakraborty@f5.com>
2023-02-10 08:00:27 -0800

    OktaUpdaedWorkingRunScriptAdded

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -classpath target/emp-connector-0.0.1-SNAPSHOT-phat.jar com.f5.salesforce.emp.connector.DevLoginAsynchronousEventProcessing https://f5--test.my.salesforce.com chakraborty13@gmail.com ahiPara12$ /event/Partner_LMS__e -1
