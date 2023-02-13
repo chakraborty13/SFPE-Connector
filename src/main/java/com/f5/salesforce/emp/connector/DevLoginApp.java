c35c6c1803de7a964684f98ee27656f7147253aa
Satyajit Chakraborty <sa.chakraborty@f5.com>
2023-02-12 20:32:02 -0800

    env-variables-replaced-command-line-parameters-works

--- src/main/java/com/f5/salesforce/emp/connector/DevLoginApp.java
+++ src/main/java/com/f5/salesforce/emp/connector/DevLoginApp.java
@@ -18,6 +18,7 @@ import com.f5.salesforce.emp.bayeux.BayeuxParameters;
 import com.f5.salesforce.emp.bayeux.EmpConnector;
 import com.f5.salesforce.emp.bayeux.LoginHelper;
 import com.f5.salesforce.emp.bayeux.TopicSubscription;
+import com.f5.salesforce.emp.connector.properties.EnvMap;
 
 import static org.cometd.bayeux.Channel.*;
 
@@ -31,58 +32,67 @@ import org.slf4j.LoggerFactory;
  * @since API v37.0
  */
 public abstract class DevLoginApp {
-	
+
 	private final Logger logger = LoggerFactory.getLogger(DevLoginApp.class);
+	private static EnvMap envMap = new EnvMap();
+	
+	private static final String sfUrl = envMap.getEnv("SF_URL");
+	private static final String sfUsername = envMap.getEnv("SF_USERNAME");
+	private static final String sfCreds = envMap.getEnv("SF_CREDS");
+	private static final String sfTopic = envMap.getEnv("SF_TOPIC");
+	private static final long sfReplayFrom = Long.parseLong(envMap.getEnv("SF_REPLAY_FROM"));	
+	
+	public void processEvents(String[] argv) throws Throwable {
+		logger.info("SF parameters receiveds \n{} \n{} \n{} \n{} {}",sfUrl,sfUsername,sfCreds,sfTopic,sfReplayFrom);
+		logger.info("LOGGER TEST FROM DebLoginApp");
+		
+		/**
+		if (argv.length < 4 || argv.length > 5) {
+			System.err.println("Usage: DevLoginExample url username password topic [replayFrom]");
+			System.exit(1);
+		}
+		**/
 
-    public void processEvents(String[] argv) throws Throwable {
-    	logger.info("LOGGER TEST FROM DebLoginApp");
-        if (argv.length < 4 || argv.length > 5) {
-            System.err.println("Usage: DevLoginExample url username password topic [replayFrom]");
-            System.exit(1);
-        }
+		BearerTokenProvider tokenProvider = new BearerTokenProvider(() -> {
+			try {
+				return LoginHelper.login(new URL(sfUrl), sfUsername,sfCreds );
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		});
 
-        BearerTokenProvider tokenProvider = new BearerTokenProvider(() -> {
-            try {
-                return LoginHelper.login(new URL(argv[0]), argv[1], argv[2]);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        });
+		BayeuxParameters params = tokenProvider.login();
 
-        BayeuxParameters params = tokenProvider.login();
+		EmpConnector connector = new EmpConnector(params);
+		LoggingListener loggingListener = new LoggingListener(true, true);
 
-        EmpConnector connector = new EmpConnector(params);
-        LoggingListener loggingListener = new LoggingListener(true, true);
+		connector.addListener(META_HANDSHAKE, loggingListener).addListener(META_CONNECT, loggingListener)
+				.addListener(META_DISCONNECT, loggingListener).addListener(META_SUBSCRIBE, loggingListener)
+				.addListener(META_UNSUBSCRIBE, loggingListener);
 
-        connector.addListener(META_HANDSHAKE, loggingListener)
-                .addListener(META_CONNECT, loggingListener)
-                .addListener(META_DISCONNECT, loggingListener)
-                .addListener(META_SUBSCRIBE, loggingListener)
-                .addListener(META_UNSUBSCRIBE, loggingListener);
+		connector.setBearerTokenProvider(tokenProvider);
 
-        connector.setBearerTokenProvider(tokenProvider);
+		connector.start().get(5, TimeUnit.SECONDS);
 
-        connector.start().get(5, TimeUnit.SECONDS);
+		long replayFrom = EmpConnector.REPLAY_FROM_EARLIEST;
+		if (argv.length == 5) {
+			replayFrom = Long.parseLong(argv[4]);
+		}
+		TopicSubscription subscription;
+		try {
+			subscription = connector.subscribe(sfTopic, sfReplayFrom, getConsumer()).get(5, TimeUnit.SECONDS);
+		} catch (ExecutionException e) {
+			System.err.println(e.getCause().toString());
+			System.exit(1);
+			throw e.getCause();
+		} catch (TimeoutException e) {
+			System.err.println("Timed out subscribing");
+			System.exit(1);
+			throw e.getCause();
+		}
 
-        long replayFrom = EmpConnector.REPLAY_FROM_EARLIEST;
-        if (argv.length == 5) {
-            replayFrom = Long.parseLong(argv[4]);
-        }
-        TopicSubscription subscription;
-        try {
-            subscription = connector.subscribe(argv[3], replayFrom, getConsumer()).get(5, TimeUnit.SECONDS);
-        } catch (ExecutionException e) {
-            System.err.println(e.getCause().toString());
-            System.exit(1);
-            throw e.getCause();
-        } catch (TimeoutException e) {
-            System.err.println("Timed out subscribing");
-            System.exit(1);
-            throw e.getCause();
-        }
+		logger.info(String.format("Subscribed: %s", subscription));
+	}
 
-        logger.info(String.format("Subscribed: %s", subscription));
-    }
-
-    public abstract Consumer<Map<String, Object>> getConsumer();
+	public abstract Consumer<Map<String, Object>> getConsumer();
 }
--- src/main/java/com/f5/salesforce/emp/connector/properties/EnvMap.java
+++ src/main/java/com/f5/salesforce/emp/connector/properties/EnvMap.java
@@ -3,25 +3,24 @@ package com.f5.salesforce.emp.connector.properties;
 import java.util.Map;
 
 public class EnvMap {
-  public void listEnv() {
-    Map<String, String> env = System.getenv();
-    for (String envName : env.keySet()) {
-      System.out.format("%s=%s%n",
-          envName,
-          env.get(envName));
-    }
-  }
+	public void listEnv() {
+		Map<String, String> env = System.getenv();
+		for (String envName : env.keySet()) {
+			System.out.format("%s=%s%n", envName, env.get(envName));
+		}
+	}
 
-  public String getEnv(String key) {
-    String value = null;
-    Map<String, String> env = System.getenv();
-    for (Map.Entry<String, String> entry : env.entrySet()) {
-      //logger.info("Key = " + entry.getKey() + ", Value = " + entry.getValue());
-      if (entry.getKey().equals(key)) {
-        value = entry.getValue();
-      }
-    }
-    return value;
-  }
+	public static String getEnv(String key) {
+
+		final String value;
+		Map<String, String> env = System.getenv();
+		for (Map.Entry<String, String> entry : env.entrySet()) {
+			// logger.info("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+			if (entry.getKey().equals(key)) {
+				value = entry.getValue();
+				return value;
+			}
+		}
+		return null;
+	}
 }
-
